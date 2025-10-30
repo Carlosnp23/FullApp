35ce4e512b29bdc67456695a6b549f97a6284c17
Carlos Norambuena <carlos.norambuena.p@gmail.com>
2025-10-30 17:28:33 -0400

    add AppDbContext

---
+++ FullApp.Api/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using FullApp.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullApp.Api.Data
+{
+    // EF Core DbContext. Maps entities to database tables.
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        // Model configuration
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Unique index on email
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            // Additional configurations can go here
+        }
+    }
+}
